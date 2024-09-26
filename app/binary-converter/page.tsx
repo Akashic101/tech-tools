95c7689526917ad8c614f4fb8c614958a769027b
SydneyAntoni <sydney.antoni@xapling.de>
2024-09-26 15:11:41 +0200

    simplified logic

--- app/binary-converter/page.tsx
+++ app/binary-converter/page.tsx
@@ -1,49 +1,45 @@
 'use client';
 
-import { Center, Select, Stack, TextInput } from '@mantine/core';
+import { Center, Select, Stack, Textarea, TextInput } from '@mantine/core';
 import { useEffect, useState } from 'react';
-
-const baseValues = [
-	{ value: 2, label: 'Binary' },
-	{ value: 8, label: 'Octal' },
-	{
-		value: 10,
-		label: 'Decimal',
-	},
-	{ value: 16, label: 'Hexadecimal' },
-];
 
 export default function BinaryConverter() {
 	const [textInput, setTextInput] = useState<string>('');
 	const [binary, setBinary] = useState<string>('');
 	const [octal, setOctal] = useState<string>('');
-	const [deximal, setDecimal] = useState<string>('');
+	const [decimal, setDecimal] = useState<string>('');
 	const [hex, setHex] = useState<string>('');
-	const [base, setBase] = useState<number>(2);
 
 	function textToNumber(text: string, base: number) {
-		console.log(text, base);
-		let binary = '';
+		let convertedValue = '';
 		for (let i = 0; i < text.length; i++) {
-			binary += text.charCodeAt(i).toString(base);
+			convertedValue += text.charCodeAt(i).toString(base);
 		}
-		return binary;
+		return convertedValue;
 	}
 
 	useEffect(() => {
-		textToNumber(textInput, base);
-	}, [textInput, base]);
+// Update all converted values based on text inputsetBinary(textToNumber(textInput, 2));
+		setBinary(textToNumber(textInput, 2));
+		setOctal(textToNumber(textInput, 8));
+		setDecimal(textToNumber(textInput, 10));
+		setHex(textToNumber(textInput, 16));
+	}, [textInput]);
+
 
 	return (
 		<Center>
 			<Stack w={'50%'} align="stretch" justify="center" gap="md">
-				<Select data={baseValues} value={base} onChange={(value) => setBase(value)} />
-				<TextInput
+				<Textarea
 					label="Text"
 					value={textInput}
 					onChange={(event) => setTextInput(event.currentTarget.value)}
+					autosize
 				/>
-				<TextInput label="Binary" value={binary} readOnly />
+				<Textarea label="Binary" value={binary} readOnly autosize />
+				<Textarea label="Octal" value={octal} readOnly autosize/>
+				<Textarea label="Decimal" value={decimal} readOnly autosize/>
+				<Textarea label="Hexadecimal" value={hex} readOnly autosize/>
 			</Stack>
 		</Center>
 	);
